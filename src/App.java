91cd6561d02761b9888ef5bb504f5134ae1ac381
emekaze <marks.dev@hotmail.com>
2025-04-09 21:36:37 -0300

    implementacao de realizar saque

--- src/App.java
+++ src/App.java
@@ -32,6 +32,8 @@ public class App {
         double saldoAtual = 0.0d;
         double totalDeposito = 0.0d;
         int qtyDeposito = 0;
+        int qtySaques = 0;
+        boolean valorValidacao = false;
 
         int option = 0;
 
@@ -62,6 +64,41 @@ public class App {
 
                         break;
                     case 3:
+                        System.out.print("\033\143");
+                        System.out.println("========== REALIZAR SAQUE ==========\n");
+                        System.out.println("Digite o valor a ser sacado!");
+
+                        float valorSaque = 0f;
+                        int notas;
+
+                        while (valorValidacao == false) {
+                            if (input.hasNextFloat()) {
+                                valorSaque = input.nextFloat();
+                                input.nextLine();
+
+                                if (valorSaque % 2 != 0) {
+                                    System.out.println("ERRO! Notas disponiveis: 100, 50, 20, 10, 5, 2.");
+
+                                } else if (valorSaque <= 0) {
+                                    System.out.println("Digite um valor que seja maior que zero!");
+                                } else if (valorSaque > saldoAtual) {
+                                    System.out.println("Impossivel sacar!! Saldo insuficiente");
+                                } else {
+                                    valorValidacao = true;
+                                    saldoAtual -= valorSaque;
+                                    qtySaques++;
+
+                                    System.out.print("\033\143");
+                                    System.out.format("Saque de R$%.2f realizado com sucesso!\n", valorSaque);
+                                    System.out.format("Saldo atual: R$%.2f\n\n", saldoAtual);
+                                }
+
+                            } else {
+                                System.out.println("ERRO! Digite um valor numerico!");
+                            }
+                        }
+                        System.out.println("Pressione ENTER para voltar ao menu...");
+                        input.nextLine();
 
                         break;
                     case 4:
@@ -96,7 +133,7 @@ public class App {
                         saldoInicial = input.nextFloat();
                         saldoAtual = saldoInicial;
                         input.nextLine();
-
+                        
                         System.out.print("\033\143");
                         if (saldoInicial != 0.0d && saldoInicial > 0 && saldoAtual == saldoInicial) {
                             contaExists = true;
